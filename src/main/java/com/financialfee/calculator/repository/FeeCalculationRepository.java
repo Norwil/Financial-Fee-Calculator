e63561e7c58765e6036aef4183435869fad9353f
Norwil <emretokluk@gmail.com>
2025-04-28 22:39:23 +0200

    FeeCalculationRepository updated

--- src/main/java/com/financialfee/calculator/repository/FeeCalculationRepository.java
+++ src/main/java/com/financialfee/calculator/repository/FeeCalculationRepository.java
@@ -2,6 +2,8 @@ package com.financialfee.calculator.repository;
 
 import com.financialfee.calculator.model.FeeCalculation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeeCalculationRepository extends JpaRepository<FeeCalculation, Long> {
 }
